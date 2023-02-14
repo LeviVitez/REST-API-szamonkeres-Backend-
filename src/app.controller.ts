ddd8a236695bcb9ee1212652273e227d1a8be6ed
LeviVitez <bmwyolo555@gmail.com>
2023-02-14 09:53:23 +0100

    get delete post

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,15 @@
-import { Body, Controller, Get, Post, Render } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Render,
+} from '@nestjs/common';
 import { DataSource } from 'typeorm';
 import { AppService } from './app.service';
+import TortaData from './TortaData.dto';
 
 @Controller()
 export class AppController {
@@ -13,5 +22,23 @@ export class AppController {
   @Render('index')
   index() {
     return { message: 'Welcome to the homepage' };
+  }
+  @Get('TortaData')
+  async tortaLista() {
+    const TortaRepo = this.dataSource.getRepository(TortaData);
+    return await TortaRepo.find();
+  }
+
+  @Delete('TortaData/:id')
+  async deleteTorta(@Param('id') id: number) {
+    const TortaRepo = this.dataSource.getRepository(TortaData);
+    TortaRepo.delete(id);
+  }
+
+  @Post('TortaData')
+  async createTorta(@Body() Torta: TortaData) {
+    Torta.id = undefined;
+    const TortaRepo = this.dataSource.getRepository(TortaData);
+    TortaRepo.save(Torta);
   }
 }
